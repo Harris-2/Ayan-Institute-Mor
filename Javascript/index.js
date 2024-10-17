06c4ff151815228c45a67c07d6702c45b149b4d0
Harris-2 <harrisweb.15@gmail.com>
2024-10-17 11:53:39 +0500

    intro to JS

---
+++ Javascript/index.js
@@ -0,0 +1,48 @@
+// Let Variable
+
+let num = 10;
+
+let age = 24;
+console.log(age);
+
+let a = '500';
+
+let str = 'This is a string data type';
+console.log(str);
+
+let city;
+city = 'lahore';
+console.log(city);
+
+let flo = 3.1415; 
+
+let bool = true;
+
+let boolean = false;
+
+let fruits = ['apple', 'mango', 'orange']
+
+let obj = {
+    name : 'haris',
+    age : 24,
+    city : 'Lahore'
+}
+
+let abc = null;
+console.log(abc);
+
+let car;
+console.log(car);
+
+
+// Const variable
+
+const number = 50;
+
+const name = 'Haris';
+
+// var Variable
+
+var int = 100;
+
+var area = 'Mansoora';
